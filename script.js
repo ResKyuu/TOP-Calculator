3216774bbee1f168b8b677128faa666fc7c6f6de
ResKyuu <73728388+ResKyuu@users.noreply.github.com>
2025-03-15 17:26:34 +0000

    Finished the Project Calculator App
    Created and finalized the logic for all calculations with Javascript
    Added a footer in html
    Adjusted the Calculator CSS styling to look more modern

--- script.js
+++ script.js
@@ -1,16 +1,93 @@
-function add(a, b) {
-  return a + b;
-}
+// Change from querySelector to querySelectorAll
+let numbers = document.querySelectorAll(".numBtn");
+let operators = document.querySelectorAll(".operatorBtn");
+let equal = document.querySelector(".equalsBtn");
+let clear = document.querySelector(".clearBtn");
 
-function sub(a, b) {
-  return a - b;
-}
+let display = document.querySelector("input");
 
-function mul(a, b) {
-  return a * b;
-}
+let firstNumber = 0;
+let secondNumber = 0;
+let currentOperator = "";
 
-function div(a, b) {
-  return a / b;
-}
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return sub(a, b);
+    case "x":
+      return mul(a, b);
+    case "÷":
+      return div(a, b);
+    default:
+      return "Invalid operator";
+  }
 
+  function add(a, b) {
+    return a + b;
+  }
+
+  function sub(a, b) {
+    return a - b;
+  }
+
+  function mul(a, b) {
+    return a * b;
+  }
+
+  function div(a, b) {
+    if (b === 0) {
+      return "Can't divide by 0";
+    } else {
+      return a / b;
+    }
+  }
+}
+// Creates a Event Listener for each number button
+numbers.forEach(function (number) {
+  number.addEventListener("click", function () {
+    display.value += number.textContent;
+    if (currentOperator === "") {
+      if (firstNumber === 0) {
+        firstNumber = number.textContent;
+      } else {
+        firstNumber += number.textContent;
+      }
+    } else {
+      if (secondNumber === 0) {
+        secondNumber = number.textContent;
+      } else {
+        secondNumber += number.textContent;
+      }
+    }
+  });
+});
+
+// Creates a Event Listener for each operator button
+operators.forEach(function (operator) {
+  operator.addEventListener("click", function () {
+    if (firstNumber !== 0 && currentOperator === "") {
+      currentOperator = operator.textContent;
+      display.value += operator.textContent;
+    }
+  });
+});
+
+// Creates a Event Listener for the equal button
+equal.addEventListener("click", function () {
+  if (firstNumber !== 0 && secondNumber !== 0) {
+    display.value = operate(currentOperator, +firstNumber, +secondNumber);
+    firstNumber = display.value;
+    secondNumber = 0;
+    currentOperator = "";
+  }
+});
+
+// Creates a Event Listener for the clear button
+clear.addEventListener("click", function () {
+  display.value = "";
+  firstNumber = 0;
+  secondNumber = 0;
+  currentOperator = "";
+});
